fc14146b134be53a482ef503be0e011d9e5416af
ashpex <ashpex@pm.me>
2022-03-07 19:27:49 +0700

    Simple input/output, delete, update students

--- src/com/ashpex/Main.java
+++ src/com/ashpex/Main.java
@@ -1,8 +1,116 @@
 package com.ashpex;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        ArrayList<Student> studentArrayList = new ArrayList<Student>();
+
+/*        try {
+            readDataFromBinaryFile(studentArrayList);
+        }catch (IOException e){
+            System.err.println("Error reading data from file");
+        }*/
+        int choice;
+        Scanner Input = new Scanner(System.in);
+        do{
+            showMenu();
+            choice = Integer.parseInt(Input.nextLine());
+            switch (choice){
+                case 1:
+                    System.out.println("Enter number of students to add: ");
+                    int n = Integer.parseInt(Input.nextLine());
+                    for(int i = 0; i < n; i++){
+                        Student std = new Student();
+                        std.inputInfo();
+                        studentArrayList.add(std);
+                        writeDataToBinaryFile(studentArrayList);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter student id to update: ");
+                    String id = Input.nextLine();
+                    boolean found = false;
+                    for(int i = 0; i < studentArrayList.size(); i++){
+                        if(studentArrayList.get(i).getId().equals(id)){
+                            studentArrayList.get(i).updateInfo();
+                            found = true;
+                        }
+                    }
+                    if(!found){
+                        System.out.println("Student not found");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter student id to delete: ");
+                    found = false;
+                    id = Input.nextLine();
+                    for(int i = 0; i < studentArrayList.size(); i++){
+                        if(studentArrayList.get(i).getId().equals(id)){
+                            studentArrayList.remove(i);
+                            found = true;
+                        }
+                    }
+                    if (!found){
+                        System.out.println("Student not found");
+                    }
+                    else {
+                        System.out.println("Student deleted");
+                    }
+                    break;
+                case 4:
+                    if(studentArrayList.size() == 0){
+                        System.out.println("There are no students");
+                    }
+                    for(Student std : studentArrayList){
+                        std.showInfo();
+                    }
+                    break;
+                case 5:
+                    System.out.println("Enter file name to import: ");
+                    String fileName = Input.nextLine();
+                    studentArrayList = Student.importStudent(fileName);
+                    System.out.println("Import success");
+                    break;
+                case 6:
+                    System.out.println("Enter file name to export: ");
+                    fileName = Input.nextLine();
+                    Student.exportStudent(studentArrayList, fileName);
+                    System.out.println("Export success");
+                    break;
+                case 7:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }while (choice != 7);
+    }
+
+    static void showMenu(){
+        System.out.println("==============================");
+        System.out.println("1. Add a student");
+        System.out.println("2. Update a student");
+        System.out.println("3. Delete a student");
+        System.out.println("4. Show all students");
+        System.out.println("5. Import students");
+        System.out.println("6. Export students");
+        System.out.println("7. Exit");
+        System.out.println("Enter your choice: ");
+    }
+
+
+    public static void writeDataToBinaryFile(ArrayList<Student> studentArrayList) throws IOException {
+        Student.exportStudent(studentArrayList, "student.dat");
+
+    }
+
+    public static void readDataFromBinaryFile(ArrayList<Student> studentArrayList) throws IOException {
+        studentArrayList = Student.importStudent("student.dat");
+
     }
 }
--- src/com/ashpex/Student.java
+++ src/com/ashpex/Student.java
@@ -1,5 +1,12 @@
 package com.ashpex;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Student {
     String id;
     String name;
@@ -22,6 +29,61 @@ public class Student {
         this.grade = grade;
     }
 
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public static ArrayList<Student> importStudent(String fileName) {
+        Scanner Input = new Scanner(System.in);
+        ArrayList<Student> list = new ArrayList<>();
+        List<List<String>> records = new ArrayList<>();
+        while(Input.hasNextLine()) {
+            records.add(getRecordFromLine(Input.nextLine()));
+        }
+        for (List<String> record : records) {
+            Student student = new Student(record.get(0), record.get(1), record.get(2), record.get(3), record.get(4), record.get(5), Float.parseFloat(record.get(6)));
+            list.add(student);
+        }
+        return list;
+    }
+
+    private static List<String> getRecordFromLine(String nextLine) {
+        List<String> record = new ArrayList<>();
+        String[] fields = nextLine.split(",");
+        for (String field : fields) {
+            record.add(field);
+        }
+        return record;
+    }
+
+    public static void exportStudent(ArrayList<Student> studentArrayList, String fileName) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        //fileName = fileName + ".csv";
+        FileWriter fileWriter = new FileWriter(fileName);
+
+/*        String title = stringBuilder.append("id,name,gender,image,address,grade,note\n").toString();
+        fileWriter.write(title);
+        stringBuilder.setLength(0);*/
+
+        for (Student student : studentArrayList) {
+            stringBuilder.append(student.id + ",");
+            stringBuilder.append(student.name + ",");
+            stringBuilder.append(student.gender + ",");
+            stringBuilder.append(student.image + ",");
+            stringBuilder.append(student.address + ",");
+            stringBuilder.append(student.grade + ",");
+            stringBuilder.append(student.note + "\n");
+
+            String content = stringBuilder.toString();
+            fileWriter.write(content);
+            stringBuilder.setLength(0);
+        }
+        fileWriter.flush();
+        fileWriter.close();
+        System.out.println("Done");
+    }
+
+
     public String getId() {
         return id;
     }
@@ -42,8 +104,15 @@ public class Student {
         return gender;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
+    public Boolean setGender(String gender) {
+        if (gender == "male" || gender == "female"){
+            System.err.println("Gender must be male or female");
+            return false;
+        }
+        else{
+            this.gender = gender;
+            return true;
+        }
     }
 
     public String getImage() {
@@ -74,7 +143,87 @@ public class Student {
         return grade;
     }
 
-    public void setGrade(Float grade) {
-        this.grade = grade;
+    public boolean setGrade(Float grade) {
+        if(grade < 0 || grade > 10) {
+            System.err.println("Grade must be between 0 and 10");
+            return false;
+        }else{
+            this.grade = grade;
+            return true;
+        }
+    }
+
+    public void inputInfo() {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Input information of student");
+
+        System.out.println("ID: ");
+        this.id = Input.nextLine();
+
+        System.out.println("Name: ");
+        this.name = Input.nextLine();
+
+
+        System.out.println("Gender (male/female): ");
+        while(true){
+            String temp = Input.nextLine();
+            boolean check = setGender(temp);
+            if(check) break;
+        }
+
+        System.out.println("Image: ");
+        this.image = Input.nextLine();
+
+        System.out.println("Address: ");
+        this.address = Input.nextLine();
+
+        System.out.println("Grade: ");
+        while(true){
+            float temp = Float.parseFloat(Input.nextLine());
+            boolean check = setGrade(temp);
+            if(check) break;
+        }
+
+        System.out.println("Note: ");
+        this.note = Input.nextLine();
+    }
+
+    public void updateInfo() {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Update information of student (press enter if there is no change" );
+
+        System.out.println("ID: ");
+        String id = Input.nextLine();
+        if(id.length() > 0) this.id = id;
+
+        System.out.println("Name: ");
+        String name = Input.nextLine();
+        if(name.length() > 0) this.name = name;
+
+        System.out.println("Gender: ");
+        String gender = Input.nextLine();
+        if(gender.length() > 0) this.gender = gender;
+
+        System.out.println("Image: ");
+        String image = Input.nextLine();
+        if(image.length() > 0) this.image = image;
+
+        System.out.println("Address: ");
+        String address = Input.nextLine();
+        if(image.length() > 0) this.address = address;
+
+        System.out.println("Grade: ");
+        Float grade = Float.parseFloat(Input.nextLine());
+        if(grade > 0) this.grade = grade;
+    }
+
+    public void showInfo(){
+        System.out.println("==========================");
+        System.out.println("ID: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Address: " + this.address);
+        System.out.println("Grade: " + this.grade);
+        System.out.println("Note: " + this.note);
     }
 }
